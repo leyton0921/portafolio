d759d0e86062213aaca4bfb579f2771e5971cd2e
leyton0921 <leytoncarmonaisabella@gmail.com>
2024-10-17 11:49:19 -0500

    color

--- src/components/options/options.tsx
+++ src/components/options/options.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './options.module.scss';
 import Button from '../UI/button/button';
 import { HiTranslate } from "react-icons/hi";
-import { FaSun } from "react-icons/fa";
+import { FaSun, FaMoon } from "react-icons/fa";
 
- const Options:React.FC= ()=> {
+const LanguageModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <Button className={styles.modalButton} onClick={onClose}>Inglés</Button>
+        <Button className={styles.modalButtonscudary} onClick={onClose}>Español</Button>
+      </div>
+    </div>
+  );
+};
+
+const Options: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+    document.body.classList.toggle('dark-theme', !isDarkTheme);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.ContainerIcons}>
-      <Button className={styles.button}><HiTranslate size={20}/></Button>
-      <Button className={styles.button}><FaSun size={20}/></Button>
+        <Button className={styles.button} onClick={toggleModal}>
+          {isDarkTheme ? <HiTranslate size={20} color='#ffff'/> : <HiTranslate size={20} />}
+        </Button>
+        <Button className={styles.button} onClick={toggleTheme}>
+          {isDarkTheme ? <FaSun size={20} color='#ffff' /> : <FaMoon size={20} />}
+        </Button>
       </div>
+      
+      {isModalOpen && <LanguageModal onClose={toggleModal} />}
     </div>
-  )
-}
+  );
+};
 
-export default Options
+export default Options;
